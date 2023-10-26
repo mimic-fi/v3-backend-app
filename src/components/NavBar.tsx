f8fe12adbec564501b79bdf48834743c74fbe1db
delfipolito <delfinapolito@gmail.com>
2023-10-26 14:43:11 -0300

    fix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -47,6 +47,7 @@ const NavbarSection = styled.section`
      padding: 0 15px;
      color: white;
      text-decoration: none;
+     font-weight: bold;
    }
 `
 
@@ -66,6 +67,7 @@ const NavbarLink = styled.div`
     font-weight: 700;
     color: #6f5ce6;
     font-family: 'DMSansBold';
+
   }
 `
 
