832778f8d05833feb111f79e11fe1349393c8dfe
delfipolito <delfinapolito@gmail.com>
2023-10-26 11:27:34 -0300

    styles

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import styled from 'styled-components'
+import bg from './assets/bg.png'
+import logo from './assets/logo.svg'
 
 const URL = process.env.REACT_APP_SERVER_BASE_URL
 
 interface LoginFormProps {
   onLogin: () => void
   showError: (message: string) => void
+  background: string
 }
 
 export default function Login({ onLogin, showError }: LoginFormProps) {
@@ -19,18 +23,145 @@ export default function Login({ onLogin, showError }: LoginFormProps) {
       const token: string = response.data.token
       localStorage.setItem('token', token)
       onLogin()
-    } catch (error: any) {
-      console.log(error)
-      showError(error.response.data.content.message)
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        console.log(error)
+        showError(error.response.data.content.message)
+      } else {
+        console.log('Error desconocido:', error)
+        showError('Error desconocido')
+      }
     }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h3>Login</h3>
-      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button type="submit">Login</button>
-    </form>
+    <>
+      <Navbar />
+      <LoginSection>
+        <Form onSubmit={handleSubmit} background={bg}>
+          <TitleM>Log in</TitleM>
+          <input
+            type="email"
+            placeholder="Email"
+            autoComplete="username"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            autoComplete="current-password"
+            onChange={e => setPassword(e.target.value)}
+          />
+          <ButtonColor type="submit">Login</ButtonColor>
+        </Form>
+      </LoginSection>
+    </>
   )
 }
+
+const LoginSection = styled.div`
+  height: calc(100vh - 80px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Form = styled.form`
+  display: block;
+  background-size: cover;
+  background: url(${props => props.background}) no-repeat 10%;
+  padding: 40px 200px;
+  border-radius: 20px;
+  background-size: cover;
+  input {
+    display: block;
+    margin-bottom: 20px;
+    width: 300px;
+    height: 40px;
+    padding: 0 10px;
+    border-radius: 7px;
+    border: 0px;
+    font-family: 'DMSans';
+    font-size: 15px;
+  }
+`
+
+const ButtonColor = styled.button`
+  display: inline-block;
+  width: auto;
+  padding: 10px 24px;
+  border-radius: 16px;
+  border: 0px;
+  background: #6f5ce6;
+  font-family: 'DMSans';
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 150%;
+  text-align: center;
+  cursor: pointer;
+  color: #ffffff;
+  margin-top: 20px;
+  &:hover {
+    background: #582ea0;
+  }
+`
+
+const TitleM = styled.h1`
+  color: white;
+  font-size: 24px;
+  margin: 0;
+  font-family: 'Visby';
+  font-style: normal;
+  line-height: 120%;
+  font-size: 40px;
+  margin-bottom: 50px;
+  @media only screen and (max-width: 800px) {
+    font-size: 32px;
+  }
+`
+
+export function Navbar() {
+  return (
+    <NavbarSection>
+      <NavbarContainer>
+        <img alt="logo" src={logo} />
+
+        <NavbarLink>
+          Learn more in{' '}
+          <a target="_blank" href="https://mimic.fi" rel="noreferrer">
+            Mimic.fi
+          </a>
+        </NavbarLink>
+      </NavbarContainer>
+    </NavbarSection>
+  )
+}
+
+const NavbarSection = styled.section`
+  z-index: 100;
+  width: 100%;
+  margin: auto;
+  position: sticky;
+  background: #12141a;
+`
+
+const NavbarContainer = styled.div`
+  margin: 0 32px;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding 0;
+`
+
+const NavbarLink = styled.div`
+  color: #a5a1b7;
+  font-feature-settings: 'clig' off, 'liga' off;
+  a {
+    font-weight: 700;
+    color: #6f5ce6;
+    font-family: 'DMSansBold';
+  }
+`
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import logo from './assets/mimic-admin.svg'
 
 interface NavBarProps {
   onLogout: () => void
@@ -12,14 +14,76 @@ export default function NavBar({ onLogout }: NavBarProps) {
   }
 
   return (
-    <div className="nav-bar">
-      <Link to="/api">API</Link>
-      <Link to="/price-oracle">Price Oracle</Link>
-      <Link to="/relayer-executor">Relayer Executor</Link>
-      <Link to="/token-monitor">Token Monitor</Link>
-      <Link to="/token-registry">Token Registry</Link>
-      <Link to="/web3">Web3</Link>
-      <button onClick={handleLogout}>Logout</button>
-    </div>
+    <NavbarSection>
+      <NavbarContainer>
+        <img alt="logo" src={logo} />
+        <div className="nav-bar">
+          <Link to="/api">API</Link>
+          <Link to="/price-oracle">Price Oracle</Link>
+          <Link to="/relayer-executor">Relayer Executor</Link>
+          <Link to="/token-monitor">Token Monitor</Link>
+          <Link to="/token-registry">Token Registry</Link>
+          <Link to="/web3">Web3</Link>
+        </div>
+        <NavbarLink>
+          <ButtonColor onClick={handleLogout}>Logout</ButtonColor>
+        </NavbarLink>
+      </NavbarContainer>
+    </NavbarSection>
+
   )
 }
+
+
+const NavbarSection = styled.section`
+  z-index: 100;
+  width: 100%;
+  margin: auto;
+  position: sticky;
+  background: #12141a;
+
+   a {
+     padding: 0 15px;
+     color: white;
+     text-decoration: none;
+   }
+`
+
+const NavbarContainer = styled.div`
+  margin: 0 32px;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding 0;
+`
+
+const NavbarLink = styled.div`
+  color: #a5a1b7;
+  font-feature-settings: 'clig' off, 'liga' off;
+  a {
+    font-weight: 700;
+    color: #6f5ce6;
+    font-family: 'DMSansBold';
+  }
+`
+
+const ButtonColor = styled.button`
+  display: inline-block;
+  width: auto;
+  padding: 10px 24px;
+  border-radius: 16px;
+  border: 0px;
+  background: #6f5ce6;
+  font-family: 'DMSans';
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 150%;
+  text-align: center;
+  cursor: pointer;
+  color: #ffffff;
+  margin-top: 20px;
+  &:hover {
+    background: #582ea0;
+  }
+`
