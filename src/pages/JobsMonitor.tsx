1d678bcb3defda71740c7feae45c2d618a9185da
Delfina Polito <delfinapolito@gmail.com>
2024-02-08 18:08:53 -0300

    Jobs page

---
+++ src/pages/JobsMonitor.tsx
@@ -0,0 +1,126 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import bg from '../assets/bg.png';
+import { refresh } from '../utils/web3-utils';
+
+const URL = process.env.REACT_APP_SERVER_BASE_URL;
+
+interface ExecutorProps {
+  onSuccess?: () => void;
+}
+
+const Executor: React.FC<ExecutorProps> = ({  onSuccess = () => {} }) => {
+  const [chainId, setChainId] = useState('');
+  const [monitor, setMonitor] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const token = localStorage.getItem('token');
+      const response = await axios.post(
+        `${URL}/jobs/monitor`,
+        {
+          chainId,
+          monitor,
+        },
+        {
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+            'x-auth-token': `${token}`,
+          },
+        }
+      );
+
+      setMessage(`The monitor job was succesfully triggered`);
+      onSuccess();
+    } catch (error: any) {
+      if (axios.isAxiosError(error) && error.response) {
+        if (error.response?.status === 401) {
+          try {
+            await refresh();
+            await handleFormSubmit(e);
+          } catch (refreshError) {
+            console.error(`Error: Unable to refresh token. Please log in again.`);
+          }
+        }
+        setMessage(`Error: ${error.response.data.message}`);
+      } else {
+        setMessage(`Error: An unexpected error occurred`);
+      }
+    }
+  };
+
+  return (
+    <Form bg={bg} onSubmit={handleFormSubmit}>
+      {message !== '' ? (
+        <Message>
+          <span>{message}</span>
+          <span className="close" onClick={() => setMessage('')}>
+            X
+          </span>
+        </Message>
+      ) : (
+        <>
+          <div>
+            <label>Chain id</label>
+            <input
+              type="number"
+              value={chainId}
+              onChange={(e) => setChainId((e.target.value).toString())}
+              required
+            />
+          </div>
+          <div>
+            <label>Monitor</label>
+            <input
+              type="text"
+              value={monitor}
+              onChange={(e) => setMonitor(e.target.value)}
+            />
+          </div>
+          <button type="submit">Trigger</button>
+        </>
+      )}
+    </Form>
+  );
+};
+
+interface FormProps {
+  bg: string;
+}
+
+const Form = styled.form<FormProps>`
+  width: 874px;
+  margin: 50px auto 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 30px;
+  background-size: cover;
+  background: url(${(props) => props.bg}) no-repeat 10%;
+  padding: 10px 20px;
+  border-radius: 20px;
+  background-size: cover;
+  height: 115px;
+  button {
+    height: 46px;
+    margin-top: 10px;
+  }
+`;
+
+const Message = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 18px;
+  width: 100%;
+  .close {
+    cursor: pointer;
+    font-weight: bold;
+  }
+`;
+
+export default Executor;
