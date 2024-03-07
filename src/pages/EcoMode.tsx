ddf6dfa915d97ac1b26bcb5512ffbcd4381dbfd0
Delfina Polito <delfinapolito@gmail.com>
2024-03-07 11:48:49 -0300

    Timelocks

--- src/pages/EcoMode.tsx
+++ src/pages/EcoMode.tsx
@@ -5,10 +5,23 @@ import EcoModeForm from '../components/EcoModeForm';
 import CustomConfirmationModal from '../components/CustomConfirmationModal';
 import deleteIcon from '../assets/delete.png';
 import { toast } from 'react-toastify';
-import { ContainerTable } from '../utils/styles';
+import { ContainerTable, TableDetails, BackButton } from '../utils/styles';
 import Network from "../utils/Network";
 import moment from 'moment';
 import { refresh } from '../utils/web3-utils';
+
+interface Timelock {
+  name: string;
+  task: string;
+  end: string;
+  start: string;
+  timelock: {
+    allowedAt: string;
+    frequency: string;
+    mode: string;
+    window: string;
+  }
+}
 
 interface EcoMode {
   active: boolean;
@@ -18,9 +31,9 @@ interface EcoMode {
   _id: string;
   updatedAt: string;
   smartVault: {
-   address: string;
-   chainId: number;
- };
+    address: string;
+    chainId: number;
+  };
 }
 
 const URL = process.env.REACT_APP_SERVER_BASE_URL;
@@ -31,6 +44,7 @@ const EcoModes: React.FC = () => {
     setEcoModes,
   ] = useState<EcoMode[] | null>(null);
   const [customModalOpen, setCustomModalOpen] = useState(false);
+  const [timelock, setTimelock] = useState<EcoMode | null>(null);
   const [deleteParams, setDeleteParams] = useState<EcoMode | null>(null);
 
   const fetchEcoModes = async () => {
@@ -49,7 +63,7 @@ const EcoModes: React.FC = () => {
 
       setEcoModes(response.data);
     } catch (error: any) {
-      if (error.response?.status === 401) {
+      if (error.response ?.status === 401) {
         try {
           await refresh();
           await fetchEcoModes();
@@ -72,7 +86,7 @@ const EcoModes: React.FC = () => {
 
   const handleConfirmDelete = async () => {
     const token = localStorage.getItem('token');
-    if(deleteParams) {
+    if (deleteParams) {
       const url = `${URL}/relayer-executor/eco-modes/${deleteParams.smartVault.chainId}/${deleteParams.smartVault.address}`;
 
       try {
@@ -87,7 +101,7 @@ const EcoModes: React.FC = () => {
         toast.success('Eco mode successfully deleted');
 
       } catch (error: any) {
-        if (error.response?.status === 401) {
+        if (error.response ?.status === 401) {
           try {
             await refresh();
             await handleConfirmDelete();
@@ -109,67 +123,161 @@ const EcoModes: React.FC = () => {
 
   return (
     <EcoModeSection>
-      <EcoModeForm onSuccess={fetchEcoModes} />
-      {ecoModesData ? (
+      {timelock ? <><BackButton onClick={() => setTimelock(null)}>🔙</BackButton><Timelocks item={timelock} /></> :
         <>
-          <ContainerTable>
-            <thead>
-              <tr>
-                <th>Smart Vault</th>
-                <th>Network</th>
-                <th>Active</th>
-                <th>Success Speed Smooth Factor</th>
-                <th>Safe Guard Period Pct</th>
-                <th>Max Executions Per Period</th>
-                <th>Last Update</th>
-                <th></th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {ecoModesData.map((item, index) => (
-                <tr key={index}>
-                  <td>{item.smartVault.address}</td>
-                  <td><Network network={item.smartVault.chainId} width={1200} /></td>
-                  <td>
-                    {item.active ? '🟢' : '🔴'}
-                  </td>
-                  <td>
-                    {item.averageSuccessSpeedSmoothFactor}
-                  </td>
-                  <td>{item.safeGuardPeriodPct}</td>
-                  <td>{item.maximumExecutionsPerPeriod}</td>
-                  <td>
-                    {moment(item.updatedAt).format('MMMM DD, YYYY [at] HH:mm:ss')}
-                  </td>
-                  <td>
-                    <img
-                      onClick={() =>
-                        handleDeleteClick(item)
-                      }
-                      src={deleteIcon}
-                      alt="Delete"
-                    />
-                  </td>
-                  <td>
-                    view timelock
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </ContainerTable>
-          {customModalOpen && (
-            <CustomConfirmationModal
-              message="Are you sure you want to delete this token list item?"
-              onConfirm={handleConfirmDelete}
-              onCancel={handleCancelDelete}
-            />
-          )}
-        </>
-      ) : (
-        <p>Loading...</p>
-      )}
+          <EcoModeForm onSuccess={fetchEcoModes} />
+          {ecoModesData ? (
+            <>
+              <ContainerTable>
+                <thead>
+                  <tr>
+                    <th>Smart Vault</th>
+                    <th>Network</th>
+                    <th>Active</th>
+                    <th>Success Speed Smooth Factor</th>
+                    <th>Safe Guard Period Pct</th>
+                    <th>Max Executions Per Period</th>
+                    <th>Last Update</th>
+                    <th>Timelocks</th>
+                    <th></th>
+
+                  </tr>
+                </thead>
+                <tbody>
+                  {ecoModesData.map((item, index) => (
+                    <tr key={index}>
+                      <td>{item.smartVault.address}</td>
+                      <td><Network network={item.smartVault.chainId} width={1200} /></td>
+                      <td>
+                        {item.active ? '🟢' : '🔴'}
+                      </td>
+                      <td>
+                        {item.averageSuccessSpeedSmoothFactor}
+                      </td>
+                      <td>{item.safeGuardPeriodPct}</td>
+                      <td>{item.maximumExecutionsPerPeriod}</td>
+                      <td>
+                        {moment(item.updatedAt).format('MMMM DD, YYYY [at] HH:mm:ss')}
+                      </td>
+                      <td>
+                        <TableDetails onClick={() => setTimelock(item)}>Timelocks</TableDetails>
+                      </td>
+                      <td>
+                        <img
+                          onClick={() =>
+                            handleDeleteClick(item)
+                          }
+                          src={deleteIcon}
+                          alt="Delete"
+                        />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </ContainerTable>
+              {customModalOpen && (
+                <CustomConfirmationModal
+                  message="Are you sure you want to delete this token list item?"
+                  onConfirm={handleConfirmDelete}
+                  onCancel={handleCancelDelete}
+                />
+              )}
+            </>
+          ) : (
+              <p>Loading...</p>
+            )}
+        </>}
     </EcoModeSection>
+  );
+};
+
+
+const Timelocks: React.FC<{ item: EcoMode; }> = ({ item }) => {
+  const [
+    timelocks,
+    setTimelocks,
+  ] = useState<Timelock[] | null>(null);
+
+  const fetchTimelocks = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const response = await axios.get<Timelock[]>(
+        `${URL}/relayer-executor/eco-modes/${item.smartVault.chainId}/${item.smartVault.address}/timelocks`,
+        {
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-type': 'application/json',
+            'x-auth-token': `${token}`,
+          },
+        }
+      );
+      setTimelocks(response.data);
+    } catch (error: any) {
+      if (error.response ?.status === 401) {
+        try {
+          await refresh();
+          await fetchTimelocks();
+        } catch (refreshError) {
+          console.error(`Error: Unable to refresh token. Please log in again.`);
+        }
+      }
+      console.error('Token list error:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTimelocks();
+  }, []);
+
+  return (
+    <>{timelocks &&
+      <ContainerTable>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Start</th>
+            <th>End</th>
+            <th>Task</th>
+            <th>Allowed At</th>
+            <th>Frequency</th>
+            <th>Mode</th>
+            <th>Window</th>
+          </tr>
+        </thead>
+        <tbody>
+          {timelocks.map((item, index) => (
+            <tr key={index}>
+              <td>{item.name}</td>
+              <td>{new Date(item.start).toLocaleString('es-ES', {
+                  month: 'long',
+                  day: '2-digit',
+                  year: 'numeric',
+                  hour: '2-digit',
+                  minute: '2-digit',
+                  second: '2-digit',
+                  hour12: true
+                })}
+              </td>
+              <td>{new Date(item.end).toLocaleString('es-ES', {
+                  month: 'long',
+                  day: '2-digit',
+                  year: 'numeric',
+                  hour: '2-digit',
+                  minute: '2-digit',
+                  second: '2-digit',
+                  hour12: true
+                })}
+              </td>
+              <td>{item.task}</td>
+              <td>{item.timelock.allowedAt}</td>
+              <td>{item.timelock.frequency}</td>
+              <td>{item.timelock.mode}</td>
+              <td>{item.timelock.window}</td>
+            </tr>
+          ))}
+        </tbody>
+      </ContainerTable>}
+    </>
   );
 };
 
